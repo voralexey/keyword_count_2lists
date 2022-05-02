de3b422823648c7e693749f964ec599ef441f452
Alexey Vorobev <voralexey@gmail.com>
2022-05-02 12:53:08 +0200

    all non alphanumerical characters in headers are replaced by '_'. Spaces are replaced with ')' in keywords. An '_' is added before and after each keyword.

--- count_key_words.py
+++ count_key_words.py
@@ -10,6 +10,8 @@ from tqdm import tqdm # progress bar
 
 with open("key_words_plain_list_AV.txt") as keyword_list: # list_of_keywords.txt
     keywords = [keyword.rstrip() for keyword in keyword_list]
+    keywords = [word.replace(" ", "_") for word in keywords]  # replace " " with "_"
+    keywords = [("_" + word + "_") for word in keywords]  # add "_" before and after each keyword
     keywords = list(filter(None, keywords))
 
 with open("all_txt_diff_v3.out") as my_file: # file with NCBI headers (my_file.txt) test_100_headers.txt
@@ -19,10 +21,11 @@ list_dict = {keyword:0 for keyword in keywords}
 
 # regex in keywords
 for header in tqdm(headers): # shows a % progress bar
+    sub_header = "_" + re.sub(r'\W+', '_', header).rstrip("\n").lower() + "_"  # substitute all NON alphanumerics with "_"
     for keyword in list_dict.keys():
-        if re.search(re.compile(keyword), header) is not None:
+        if re.search(re.compile(keyword), sub_header) is not None:
             list_dict[keyword] += 1
-            with open('keyword_count_all.csv', 'w') as csv_file:
+            with open('keyword_count_all_regex.csv', 'w') as csv_file: # 'keyword_count_all.csv'
                 writer = csv.writer(csv_file)
                 for key, value in list_dict.items():
                     writer.writerow([key, value])
