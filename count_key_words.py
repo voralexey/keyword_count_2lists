024e586c8ecdf76e9845e567dd349501af7d4088
Alexey Vorobev <voralexey@gmail.com>
2022-05-02 12:29:45 +0200

    all non alphanumerical characters in headers are replaced by '_'. Spaces are replaced with ')' in keywords. An '_' is added before and after each keyword.

---
+++ count_key_words.py
@@ -0,0 +1,55 @@
+import csv
+import re
+from tqdm import tqdm # progress bar
+
+# keywords = ['keyword_1', 'keyword_2', 'keyword_3']
+# occurrences = []
+
+# with open("list_of_keywords.txt") as keyword_list:
+#     keywords = keyword_list.readlines()
+
+with open("key_words_plain_list_AV.txt") as keyword_list: # list_of_keywords.txt
+    keywords = [keyword.rstrip() for keyword in keyword_list]
+    keywords = list(filter(None, keywords))
+
+with open("all_txt_diff_v3.out") as my_file: # file with NCBI headers (my_file.txt) test_100_headers.txt
+    headers = my_file.readlines()
+
+list_dict = {keyword:0 for keyword in keywords}
+
+# regex in keywords
+for header in tqdm(headers): # shows a % progress bar
+    for keyword in list_dict.keys():
+        if re.search(re.compile(keyword), header) is not None:
+            list_dict[keyword] += 1
+            with open('keyword_count_all.csv', 'w') as csv_file:
+                writer = csv.writer(csv_file)
+                for key, value in list_dict.items():
+                    writer.writerow([key, value])
+
+# NO REGEX IN KEYWORDS
+# for header in tqdm(headers): # shows a % progress bar
+#     for keyword in list_dict.keys():
+#         if header.find(keyword) != -1:
+#             list_dict[keyword] += 1
+#             with open('keyword_count_all.csv', 'w') as csv_file:
+#                 writer = csv.writer(csv_file)
+#                 for key, value in list_dict.items():
+#                     writer.writerow([key, value])
+    #print(list_dict)
+
+# count = 0
+# for keyword in keywords:
+#     # count = headers.count(keyword)
+#     # occurrences.append(count)
+#
+#     for header in headers:
+#         header = header.strip().lower().split()
+#         for word in header:
+#             if word.find(keyword.lower()) != -1:
+#                 print(word)
+#                 count += 1
+#     with open('keyword_count.txt', 'a') as keyword_count:
+#         keyword_count.write(f"{keyword} - {count}")
+#
+# # print(f"{keyword} - {count}")
